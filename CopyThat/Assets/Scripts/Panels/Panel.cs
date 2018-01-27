8b0f11814ca4e529e2b0c1a2335707e481ae73e5
cjb5670 <cj.bajus@gmail.com>
2018-01-27 15:07:43 -0500

    Summary: Added Sprite Buttons and Sliders

---
+++ CopyThat/Assets/Scripts/Panels/Panel.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Reperesents the grid of button panels
+/// </summary>
+public class Panel : MonoBehaviour
+{
+
+    // MEMBERS / FIELDS
+    #region Members
+    private ModuleType[][] panelGrid;
+
+    #endregion
+
+
+    // PROPERTIES
+    #region Properties
+
+    #endregion
+
+
+    // METHODS
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    /// <summary>
+    /// Generate a new panel grid of pieces of size 24x8, and then
+    /// determine which pieces should go into the panel
+    /// </summary>
+    public void GeneratePanelGrid()
+    {
+        GeneratePanelGrid(24, 8);
+    }
+
+    /// <summary>
+    /// Generate a new panel grid with a given number of tiles, and then
+    /// determine which pieces should go into the panel
+    /// </summary>
+    /// <param name="xGridLength">Horizontal number of grid spaces</param>
+    /// <param name="yGridLength">Vertical number of grid spaces
+    /// (should be one or greater)</param>
+    public void GeneratePanelGrid(uint xGridLength, uint yGridLength)
+    {
+
+    }
+}
---
+++ CopyThat/Assets/Scripts/Panels/SwitchBehavior.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchBehavior : Module {
+
+	public Camera camera;
+	public controlState currentState;
+
+	// Use this for initialization
+	void Start ()
+	{
+		int RNG = Random.Range(0, 1);
+		if (RNG == 0)
+		{ currentState = controlState.off; }
+		else
+		{ currentState = controlState.on; }
+	}
+ 
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{
+            RaycastHit2D hit = Physics2D.Raycast(camera.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit)
+            {
+                Interact();
+            }
+        }
+	}
+
+	protected override void Interact()
+	{
+		if (currentState == controlState.on)
+		{
+			currentState = controlState.off;
+		}
+		else if (currentState == controlState.off)
+		{
+			currentState = controlState.on;
+		}
+		else
+		{
+			Debug.Log("ERROR: Switch state somehow set to " + currentState + ". Someone fucked up.");
+		}
+	}
+}
