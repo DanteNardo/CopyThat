c14cac230ff8b7c05a1d5ab1f616594facd08f2e
cjb5670 <cj.bajus@gmail.com>
2018-01-28 00:08:38 -0500

    Added Navigation prefab

---
+++ CopyThat/Assets/Scripts/Panels/MapCrossLogic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCrossLogic : MonoBehaviour {
+
+    public bool xLock;
+    public bool yLock;
+    public GameObject cursor;
+    RectTransform myPos;
+    RectTransform cursorPos;
+	// Use this for initialization
+	void Start ()
+    {
+        myPos = gameObject.GetComponent<RectTransform>();
+        cursorPos = cursor.GetComponent<RectTransform>();
+       
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (xLock)
+        {
+            myPos.localPosition = new Vector2(myPos.localPosition.x, cursorPos.localPosition.y);
+        }
+        else if (yLock)
+        {
+            myPos.localPosition = new Vector2(cursorPos.localPosition.x, myPos.localPosition.y);
+        }
+        else
+        {
+            Debug.Log("Please select an axis lock");
+        }
+    }
+}
